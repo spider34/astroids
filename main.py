2e68c2cf0ce5cdb1a0b7d7eaa689d56263ade004
spider34 <spider344@proton.me>
2025-01-19 17:50:33 +0000

    60FPS, circle sprite, draw player, add rotation

--- main.py
+++ main.py
@@ -3,18 +3,29 @@
 # throughout this file
 import pygame
 import constants
+import player
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    x = constants.SCREEN_WIDTH / 2
+    y = constants.SCREEN_HEIGHT / 2
+    p_sprite = player.Player(x, y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        p_sprite.update(dt)
+        p_sprite.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+
 
 if __name__ == "__main__":
     main()
